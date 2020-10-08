07c7aa8b74f63a1c813c35be8473c13bc3307d95
Zsofia <oroszi.zsofi@gmail.com>
2020-10-08 12:43:10 +0200

    set up file structure and basic markup

--- front-page.php
+++ front-page.php
@@ -71,7 +71,7 @@
         ?>
     </div>
 </section>
-<div id="ttr_main" class="row">
+<!-- <div id="ttr_main" class="row">
     <div id="ttr_content" class="col-lg-8 col-sm-8 col-md-8 col-xs-12">
         <div class="row">
             <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
@@ -86,6 +86,6 @@
         </div>
     </div>
     <?php get_sidebar(); ?>
-</div> -->
+</div> --> -->
 
 <?php get_footer(); ?>
--- header.php
+++ header.php
@@ -1,19 +1,26 @@
 <html>
-<head>
-<title>Tutorial theme</title>
-<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/js/jquery.js'; ?>">
-</script>
-<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/js/jquery-ui.min.js'; ?>">
-</script>
-<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/js/bootstrap.js'; ?>">
-</script>
-<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri().'/css/bootstrap.css'; ?>">
-<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-</head>
-
-<body>
-
-<div id="ttr_header" class="jumbotron">
-<h1>Daniel Krome</h1>
-</div>
-<div class="container">
+    <head>
+        <title><?php // wp_bloginfo('name'); ?></title>
+        <meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
+        <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/js/jquery.js'; ?>">
+        </script>
+        <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/js/jquery-ui.min.js'; ?>">
+        </script>
+        <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/js/bootstrap.js'; ?>">
+        </script>
+        <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri().'/css/bootstrap.css'; ?>">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&family=Secular+One&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+        
+    </head>
+    <body>     
+        <div id="ttr_header" class="">
+            <a id="logo" class="" href="<?php bloginfo('url'); ?>">
+                <h1 class="logo">L27</h1>
+            </a>
+            <nav class="navbar">
+                <?php wp_nav_menu(array('menu'=>'menu')); ?>
+            </nav>
+        </div>
+        <div class="container">
