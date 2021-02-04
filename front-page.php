9370e20f3e9b9e42e905c35ea95fc01c6e9a6f11
Zsofia <oroszi.zsofi@gmail.com>
2021-02-04 11:41:30 +0100

    fix layout bugs, display posts by cathegory

--- front-page.php
+++ front-page.php
@@ -9,53 +9,47 @@
 <section class="wrapper">
     <section id="about" class="section section__about">
         <article class="section__content section__about-content">
-            <?php 
-                $my_postid = 23;
-                $content_post = get_post($my_postid);
-                $content = $content_post->post_content;
-                $content = apply_filters('the_content', $content);
-                $content = str_replace('&nbsp;', ' ', $content);
-                echo $content;
-            ?>
+            <?php
+                $args = array('cat' => 4);
+                $category_posts = new WP_Query($args);
+                if($category_posts->have_posts()) : while       ($category_posts->have_posts()) :       $category_posts->the_post();?>
+                <p><?php the_content(); ?></p>
+                <?php endwhile; else: ?>
+            <?php endif; ?>
         </article>
     </section>
     <section id="services" class="section section__services">
         <article class="section__content section__services-content">
-            <?php 
-                $my_postid = 24;
-                $content_post = get_post($my_postid);
-                $content = $content_post->post_content;
-                $content = apply_filters('the_content', $content);
-                $content = str_replace('&nbsp;', ' ', $content);
-                echo $content;
-            ?>
+            <?php
+                $args = array('cat' => 5);
+                $category_posts = new WP_Query($args);
+                if($category_posts->have_posts()) : while       ($category_posts->have_posts()) :       $category_posts->the_post();?>
+                <p><?php the_content(); ?></p>
+                <?php endwhile; else: ?>
+            <?php endif; ?>
         </article>
     </section>
-    <?php $post = get_post(253)->post_content;
-        if(!empty($post)):?>
             <section id="extra" class="section section__extra">
                 <article class="section__content section__extra-content">
-                <?php 
-                    $my_postid = 253;
-                    $content_post = get_post($my_postid);
-                    $content = $content_post->post_content;
-                    $content = apply_filters('the_content', $content);
-                    $content = str_replace('&nbsp;', ' ', $content);
-                    echo $content;
-                ?>
+                <?php
+                    $args = array('cat' => 6);
+                    $category_posts = new WP_Query($args);
+                    if($category_posts->have_posts()) : while           
+                    ($category_posts->have_posts()) :           $category_posts->the_post();?>
+                    <p><?php the_content(); ?></p>
+                    <?php endwhile; else: ?>
+                <?php endif; ?>
                 </article>
             </section>
-        <?php endif;?>
     <section id="contact" class="section section__contact">
         <article class="section__content section__contact-content">
-            <?php 
-                $my_postid = 25;
-                $content_post = get_post($my_postid);
-                $content = $content_post->post_content;
-                $content = apply_filters('the_content', $content);
-                $content = str_replace('&nbsp;', ' ', $content);
-                echo $content;
-            ?>
+            <?php
+                $args = array('cat' => 7);
+                $category_posts = new WP_Query($args);
+                if($category_posts->have_posts()) : while       ($category_posts->have_posts()) :       $category_posts->the_post();?>
+                <p><?php the_content(); ?></p>
+                <?php endwhile; else: ?>
+            <?php endif; ?>
         </article>
     </section>
 </section>
